8fdfd6e5467e2509fb8c022cca7d3a8792ca6a9b
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2025-02-04 15:59:30 +0900

    게임 안에 게임 프로토 타입 완성

--- Neglect/Assets/Scripts/20.GamePlay/GameManager.cs
+++ Neglect/Assets/Scripts/20.GamePlay/GameManager.cs
@@ -1,14 +1,51 @@
-﻿using System;
-using Quest;
-using UniRx;
+﻿using GamePlay;
+using GamePlay.MiniGame;
+using MoreMountains.Feedbacks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public int a = 0;
-    public void Start()
+    public MiniGameBase currentMiniGame;
+    public MMF_Player test1;
+    public MMF_Player test2;
+    
+    public void Awake()
     {
-        Observable.Interval(TimeSpan.FromSeconds(1f)).Subscribe(_ => QuestManager.Instance.OnValueChange(QuestEvent.GameRank, a++));
+        // 테스트용
+        SceneUtil.AsyncAddFlappingGame(scene =>
+        {
+            // 씬 로드 되면 일단 미니게임 초기화 하고 멈추기
+            foreach (GameObject rootGameObject in scene.GetRootGameObjects())
+            {
+                var miniGameManager = rootGameObject.GetComponentInChildren<MiniGameBase>();
+                if (miniGameManager != null)
+                {
+                    miniGameManager.InitLoadedScene(scene);
+                    miniGameManager.GameStop();
+                    currentMiniGame = miniGameManager;
+                    return;
+                }
+            }
+        });
+    }
+
+    // 테스트용
+    public void MiniGameOnOff()
+    {
+        if (currentMiniGame != null)
+        {
+            if (!currentMiniGame.isGamePlay.Value)
+            {
+                currentMiniGame.GamePlay();
+                test1.PlayFeedbacks();
+            }
+            else
+            {
+                currentMiniGame.GameStop();
+                test2.PlayFeedbacks();
+            }
+        }
     }
 }
 
