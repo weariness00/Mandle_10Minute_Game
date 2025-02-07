633b70e3e0fada2779d638c72e3884d3a1c9d0bf
goms7 <36668884+goms7@users.noreply.github.com>
2025-02-07 15:21:28 +0900

    비밀번호 은행 이벤트
    비밀번호 은행 이벤트

---
+++ Neglect/Assets/Scripts/21.Quest/Container/Quest_Bank.cs
@@ -0,0 +1,26 @@
+using Quest;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest_Bank : QuestBase
+{
+    // Start is called before the first frame update
+    public override void OnCompleted()
+    {
+    }
+
+    public override void OnError(Exception error)
+    {
+    }
+
+    public override void OnNext(object value)
+    {
+    }
+
+    public override void Play()
+    {
+        base.Play();
+    }
+}
---
+++ Neglect/Assets/Scripts/21.Quest/Container/Quest_Password.cs
@@ -0,0 +1,25 @@
+using Quest;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest_Password : QuestBase
+{
+    public override void OnCompleted()
+    {
+    }
+
+    public override void OnError(Exception error)
+    {
+    }
+
+    public override void OnNext(object value)
+    {
+    }
+
+    public override void Play()
+    {
+        base.Play();
+    }
+}
