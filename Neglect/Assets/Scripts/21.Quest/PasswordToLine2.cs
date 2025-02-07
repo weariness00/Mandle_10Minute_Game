8b27b64c5cd53582b1289f1a63e55f0d15cffcde
goms7 <36668884+goms7@users.noreply.github.com>
2025-02-07 13:39:58 +0900

    pull 전 커밋
    pull 전 커밋

--- Neglect/Assets/Scripts/21.Quest/PasswordToLine2.cs
+++ Neglect/Assets/Scripts/21.Quest/PasswordToLine2.cs
@@ -12,20 +12,6 @@ using UnityEngine.UI;
 
 public class PasswordToLine2 : MonoBehaviour
 {
-
-
-    [Header("은행 화면 정보")]
-    public GameObject BankPassword;
-    public GameObject BankTransfer;
-    public GameObject BankFinish;
-    [Space]
-
-
-    [Header("이체 해야 할 정보")]
-    public string AnswerAccount;
-    public int AnswerAmount;
-    public string PassbookOwner;
-    [Space]
     [Header("정답 패스워드")]
     public List<int> AnswerPassword;
     [Space]
@@ -46,17 +32,10 @@ public class PasswordToLine2 : MonoBehaviour
     private int CurrentView = 0; //현재 화면
 
 
-    [Header("패스워드 완료 후 계좌 이체 텍스트")]
-    public TextMeshProUGUI InputAccount;
-    public TextMeshProUGUI InputAmount;
-
-    public MMF_Player pre_sign;
-
-    [Header("마지막 확인 텍스트")]
-    public TextMeshProUGUI CheckText; 
-    public TextMeshProUGUI CheckAmountText;
-
-    
+    public void Awake()
+    {
+        Init();
+    }
     //패스워드
     public void Init()
     {
@@ -64,10 +43,7 @@ public class PasswordToLine2 : MonoBehaviour
         InputPassword.Clear();
         LineClear();
     }
-    public void RePositionPointers()
-    {
 
-    }
     public void LineDraw()
     {
         for (int i = 1; i < InputPassword.Count; i++)
@@ -106,8 +82,10 @@ public class PasswordToLine2 : MonoBehaviour
         if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             IsCrack = false;
-            if(PasswordCheck())
-                ChangeView(1); // 다음화면
+            if (PasswordCheck())
+            {
+                Debug.Log("패턴 완료");
+            }
             InputPassword.Clear();
             LineClear();
 
@@ -115,8 +93,7 @@ public class PasswordToLine2 : MonoBehaviour
     }
     public void CheckSkipNumber() // 비정상적인 패스워드 입력시 수정
     {
-        
-        int BackIndex = InputPassword.Count-1;
+       int BackIndex = InputPassword.Count-1;
        if (IsSkip())
         {
             int storeIndex = InputPassword[BackIndex];
@@ -169,6 +146,8 @@ public class PasswordToLine2 : MonoBehaviour
 
         return flag;
     }
+
+
     public bool StartCrack()
     {
         int StartNum = DetectedPoint(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
@@ -208,80 +187,4 @@ public class PasswordToLine2 : MonoBehaviour
     /// 패스워드
 
    
-    public void InputComplete() //입력 정보 확인 
-    {
-        string inputText = InputAccount.text.Trim();
-        inputText = Regex.Replace(inputText, @"\u200B", "");
-        string answerText = AnswerAccount.Trim().Replace("\n", "").Replace("\r", "");
-
-        if (inputText == answerText)// 계좌가 올바르게 되었을 경우
-        {
-            CheckTextSet();
-            ChangeView(2);  
-        }
-        else // 계좌가 올바르지 않을 경우
-        {
-            pre_sign.PlayFeedbacks(); //임시 경고 표시
-        }
-    }
-    
-    private void CheckTextSet() // 입력 정보 확인 텍스트 수정
-    {
-        CheckText.text = "Account\n" + AnswerAccount + "\nBank\n" + PassbookOwner + ".\n";
-        CheckAmountText.text = AddCommas(InputAmount.text) + "$";  
-    }
-    public static string AddCommas(string input)
-    {
-
-        input = input.Trim();
-        input= Regex.Replace(input, @"\u200B", "");
-        int length = input.Length;
-        if (length <= 3)
-            return input;
-        string result = "";
-        int count = 0;
-        for (int i = length - 1; i >= 0; i--)
-        {
-            result = input[i] + result;
-            count++;
-
-            if (count % 3 == 0 && i != 0)
-            {
-                result = "," + result;
-            }
-        }
-        return result;
-    }
-    public void BankComplete() // 입력정보 최종 확인 후 송금
-    {
-        Debug.Log("송금 완료");
-        string inputText = InputAmount.text.Trim();
-        inputText = Regex.Replace(inputText, @"\u200B", "");
-        int Amountdifference = AnswerAmount - int.Parse(inputText);
-        Debug.Log(Amountdifference + "만큼 금액 차이 발생");
-    }
-    
-
-    public void ChangeView(int index) // 임시 화면 전환
-    {
-        if(index == 0)
-        {
-            BankPassword.SetActive(true);
-            BankTransfer.SetActive(false);
-            BankFinish.SetActive(false);
-        }
-        if (index == 1)
-        {
-            BankPassword.SetActive(false);
-            BankTransfer.SetActive(true);
-            BankFinish.SetActive(false);
-        }
-        if (index == 2)
-        {
-            BankPassword.SetActive(false);
-            BankTransfer.SetActive(false);
-            BankFinish.SetActive(true);
-        }
-        CurrentView = index;
-    }
 }
