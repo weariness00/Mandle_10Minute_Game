abec304fd6128f8dd24de9418516795ce0de13aa
goms7 <36668884+goms7@users.noreply.github.com>
2025-02-11 21:49:03 +0900

    pull전 커밋
    pull전 커밋

--- Neglect/Assets/Scripts/21.Quest/Container/Quest_Password.cs
+++ Neglect/Assets/Scripts/21.Quest/Container/Quest_Password.cs
@@ -16,11 +16,33 @@ namespace Quest.Container
         {
         }
 
+        public void Start() // 테스트 
+        {
+            Password.SettingEvent("Hint : 1 2 3 4 5 6 7 8 9", "[1,2,3,4,5,6,7,8,9]");
+            Password.ClearAction += Complete;
+
+        }
+
         public override void Play()
         {
             base.Play();
             var PasswordObj = UIManager.InstantiateFromPhone(Password);
             PasswordObj.gameObject.SetActive(true);
+            //PasswordObj.SettingEvent("Hint : 1 2 3 6" , "[1,2,3,4,5,6,7,8,9]");
+
+           
+        }
+
+
+        public override void Complete()
+        {
+            Debug.Log("잠금해제");
+           // base.Complete();
+
+        }
+        public override void Ignore()
+        {
+            base.Ignore();
         }
     }
 }
