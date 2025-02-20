8d6f358e8770de7056a6b128fc42cdf92caa298c
goms7 <36668884+goms7@users.noreply.github.com>
2025-02-20 16:32:41 +0900

    알람 이벤트 추가
    알람 이벤트 추가

---
+++ Neglect/Assets/Scripts/20.GamePlay/Event/Alarm.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace GamePlay.Event
+{
+    public class Alarm : MonoBehaviour
+    {
+        public Action complete;
+        public Action ignore;
+        public TextMeshProUGUI timeText;
+
+        public void TimeSet(string text)
+        {
+            timeText.text = text;
+        }
+        public void ClearEvent()
+        {
+            if(complete != null) 
+                complete();
+            SelfDestroy();
+        }
+
+        public void IgnoreEvent()
+        {
+            if(ignore != null) 
+                ignore();
+            SelfDestroy();
+        }
+
+        public void SelfDestroy()
+        {
+
+            Destroy(gameObject);
+        }
+    }
+}
