726a6ba0d21bdb8cfe2f4051a348d03fd241498d
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2025-02-21 18:16:55 +0900

    첫 화면 연출 추가

---
+++ Neglect/Assets/Scripts/20.GamePlay/App/Home/FirstStartWindow.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GamePlay.App
+{
+    public class FirstStartWindow : MonoBehaviour
+    {
+        public EventTrigger eventTrigger;
+
+        public void Awake()
+        {
+            if (eventTrigger == null)
+                eventTrigger = gameObject.AddComponent<EventTrigger>();
+            
+            var click = eventTrigger.triggers.FirstOrDefault(e => e.eventID == EventTriggerType.PointerClick);
+            if (click == null)
+                click = new EventTrigger.Entry() { eventID = EventTriggerType.PointerClick };
+            click.callback.AddListener(data => OnPointerClick((PointerEventData)data));
+            
+            eventTrigger.triggers.Add(click);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            var narration =  FindObjectOfType<GamePlayerNarration>(true);
+            if (narration)
+            {
+                narration.StartNarration();
+            }
+            Destroy(gameObject);
+        }
+    }
+}
+
