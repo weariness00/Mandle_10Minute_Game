d14cd013d9fdc54b4d063d6d58af9aa3c73fef07
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2025-02-23 01:37:45 +0900

    러닝 게임 튜토리얼 테스트 추가

--- Neglect/Assets/Scripts/20.GamePlay/MiniGame/MiniGameTutorial.cs
+++ Neglect/Assets/Scripts/20.GamePlay/MiniGame/MiniGameTutorial.cs
@@ -24,8 +24,6 @@ public class MiniGameTutorial : MonoBehaviour
         
         leftButton.onClick.AddListener(LeftPage);
         rightButton.onClick.AddListener(RightPage);
-        
-        gameObject.SetActive(false);
     }
     
     public void LeftPage()
